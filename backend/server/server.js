3d4ef05e067b2299ea0aca51d27627a4bdaefbef
MaicolZzz <buitragomaicol23@gmail.com>
2025-11-18 19:31:50 -0500

    errore con .env

--- backend/server/server.js
+++ backend/server/server.js
@@ -1,8 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import { supabase } from "./server/db.js";
-
+import { supabase } from "./db.js";
 
 dotenv.config();
 const app = express();
